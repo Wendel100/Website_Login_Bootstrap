9625fe151d9f11f292acf19ba3e5682a963974d0
Renan <renanwendel100@gmail.com>
2024-05-17 17:19:43 -0300

    version 2.1.0

--- conectar.php
+++ conectar.php
@@ -1,29 +1,31 @@
 <?php
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-$servidor = "localhost";
-$username = "root";
-$password = "root";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servidor = "";
+    $username = "";
+    $password = "";
 
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-try{
-$conn = new PDO("mysql:host=$servidor;dbname=mydb", $username,$password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$login = "SELECT usuarios.nome FROM usuarios WHERE email =='aluno@dominio.com' and senha=='1234'";
-$login = $conn->prepare($login);
- $result = $login->fetch();
-if ($email=="aluno@dominio.com" and $senha=="1234"){
-                header('Content-Type: text/html; charset=utf-8');
-                echo "Login efetuado com sucesso!, seja bem vindo <b>";
-            }else{    
-                echo "Falha ao realizar o login!";
-            }
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    try {
+        $conn = new PDO("mysql:host=$servidor;dbname=mydb", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $login = "SELECT usuarios.nome FROM usuarios WHERE email =='renan' and senha=='1234'";
+        $login = $conn->prepare($login);
+        $result = $login->fetch();
+        if ($email == "renan" and $senha == "1234") {
+            header('Content-Type: text/html; charset=utf-8');
+            echo "Login efetuado com sucesso!, seja bem vindo <b>";
+            header("Location:cadastrar.html");
+            exit;
+        } else {
+            echo "Falha ao realizar o login!";
+        }
 
-$conn = null;
-exit();
-}catch(PDOException $e){
-    echo " Falha na conexão:". $e->getMessage();
+        $conn = null;
+        exit();
+    } catch (PDOException $e) {
+        echo " Falha na conexão:" . $e->getMessage();
+    }
 }
-   }
 exit();
 ?>
